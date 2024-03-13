876e474ec2af4fc4b2b3a2810645fc081ca86dec
JarekKrolik <madmax@onet.eu>
2024-03-13 21:09:59 +0100

    added navigation

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const burgerIcon = document.querySelector(".header-navigation-hamburger");
 const menu = document.querySelector(".header-navigation");
+const date = document.querySelector(".footer-text-date");
 
 burgerIcon.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+date.textContent = new Date().getFullYear();
